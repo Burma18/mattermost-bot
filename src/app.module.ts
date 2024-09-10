ad07b9219689bb45058c19ba380127710265872f
Burma Rysmatova <rysmatovaburma@gmail.com>
2024-09-10 14:01:18 +0600

    added vercel config

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { WebhookController } from './webhook/webhook.controller';
 import { WebhookService } from './webhook/webhook.service';
 import { WebhookModule } from './webhook/webhook.module';
+import { PingController } from './ping/ping.controller';
 
 @Module({
   imports: [WebhookModule],
-  controllers: [WebhookController],
+  controllers: [WebhookController, PingController],
   providers: [WebhookService],
 })
 export class AppModule {}
